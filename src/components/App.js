ede68355c8836f69b4bc088b0cf8b1381a02a1a5
niyazieren <48053224+niyazieren@users.noreply.github.com>
2021-08-21 13:11:52 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,45 +3,57 @@ import data from './data';
 import "./style.css";
 
 const colorMap ={
-	"noble gas":"#3232d1",
-	"alkaline earth metal": "yellow",
-	"actinide": "purple",
-	"diatomic nonmetal": "green",
 	"alkali metal":"#e3b354",
+	"alkaline earth metal": "yellow",
+	"lanthanide":"pink",
+	"actinide": "purple",
 	"transition metal":"orange",
 	"post-transition metal": "aqua",
-	"lanthanide":"pink",
+	"diatomic nonmetal": "green",
 	"metalloid":"teal",
 	"polyatomic nonmetal":"green",
+	"noble gas":"#3232d1",
 	"unknown":"#e5e4ed",
-	
 };
 
 const App = () =>{
 	return (
-		<div className="periodicTable">
-			{data.elements.map((element) => (
-				<div 
-					className="element"
-					key={element.name}
-					style={{
+		<body>	
+			
+			<div className="periodicTable">
 
-						gridColumn: element.xpos,
-						gridRow: element.ypos,
-						borderColor : colorMap[element.category],
-						backgroundColor : "black",
-						color :colorMap[element.category]
+				<div className="alkali-Metals">Alkali Metals</div>
+				<div className="alkalineEarthMetals">Alkaline Earth Metals</div>
+				<div className="lanthanides">Lanthanides</div>
+				<div className="actinides">Actinides</div>
+				<div className="transitionMetals">Transition Metals</div>
+				<div className="post-transitionmetals">Post-Transition Metals</div>
+				<div className="metalloids">Metalloids</div>
+				<div className="polyatomicNonMetals">Polyatomic Nonmetals</div>
+				<div className="nobleGas">Noble Gas</div>
+				<div className="unknowns">Unknowns</div>
+				{data.elements.map((element) => (
+					<div 
+						className="element"
+						key={element.name}
+						style={{
 
-					}}
-				>
-					<div className="symbol">{element.symbol}</div>
-					<div className="number">{element.number}</div>
-					<div className="names">{element.name}</div>
-					<div className="atomicMass">{element.atomic_mass}</div>
-				</div>
-			))
-		}
-		</div>
+							gridColumn: element.xpos,
+							gridRow: element.ypos,
+							borderColor : colorMap[element.category],
+							backgroundColor : "black",
+							color :colorMap[element.category]
+
+						}}
+					>
+						<div className="symbol">{element.symbol}</div>
+						<div className="number">{element.number}</div>
+						<div className="names">{element.name}</div>
+						<div className="atomicMass">{element.atomic_mass}</div>
+					</div>
+				))
+			}
+			</div>
 	)
 }
 
